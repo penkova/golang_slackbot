8feed85676d2d386ef6e5e5d23ba371242642244
Penkova <penkovaanastasiia@gmail.com>
2018-04-02 14:44:44 +0300

    added check welcoming prefix

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ import (
 var (
 	BotKey        Token
 	SlackClient   *slack.Client
+	ChannelInfo   *slack.ChannelInfoEvent
 	connectedUser *slack.UserDetails
+	rtm           *slack.RTM
 	welcPref      = []string{"hi", "Hi", "hello", "Hello", "Howdy", "Wassup", "Hey", "Привет", "Здравствуйте"}
 )
 
@@ -35,15 +37,15 @@ func main() {
 	// New создает Slack клиента из предоставленного токена и опций.
 	SlackClient = slack.New(BotKey.Token)
 	//NewRTM возвращает RTM, который обеспечивает полностью управляемое соединение с протоколом Real-Time Messaging от Slack на основе веб-приложений
-	rtm := SlackClient.NewRTM()
+	rtm = SlackClient.NewRTM()
 
 	//ManageConnection мы подключаемся к Slack RTM API и будет обрабатывать все входящие и исходящие события.
 	go rtm.ManageConnection()
 
-	Run(rtm)
+	Run()
 }
 
-func Run(rtm *slack.RTM) int {
+func Run() int {
 	for {
 		select {
 		case msg := <-rtm.IncomingEvents:
@@ -60,7 +62,7 @@ func Run(rtm *slack.RTM) int {
 
 			//MessageEvent представляет Slack Message (используется как тип события для входящего сообщения)
 			case *slack.MessageEvent:
-				handleMessageEvent(ev, rtm)
+				handleMessageEvent(ev)
 
 			//InvalidAuthEvent используется в случае, если мы не можем даже аутентифицироваться с помощью API
 			case *slack.InvalidAuthEvent:
@@ -71,7 +73,7 @@ func Run(rtm *slack.RTM) int {
 	}
 }
 
-func handleMessageEvent(ev *slack.MessageEvent, rtm *slack.RTM) {
+func handleMessageEvent(ev *slack.MessageEvent) {
 	channelInfo, err := SlackClient.GetChannelInfo(ev.Channel)
 	if err != nil {
 		//log.Fatalln(err)
@@ -80,33 +82,37 @@ func handleMessageEvent(ev *slack.MessageEvent, rtm *slack.RTM) {
 	fmt.Println("chanel info", channelInfo)
 	fmt.Println("chanel info 1", ev.Channel)
 
-	// поменять название функции и написать проверку в каком канале нахожусь
-	reactionBotForClient(ev, rtm)
+	//написать проверку в каком канале нахожусь
+	addReactionBotToClientMessage(ev)
 
 }
 
-func reactionBotForClient(ev *slack.MessageEvent, rtm *slack.RTM) {
+func addReactionBotToClientMessage(ev *slack.MessageEvent) {
 	botId := connectedUser.ID
 	receivedText := ev.Text
 	checkPrefBot := strings.HasPrefix(receivedText, "<@"+botId+">")
 
+	if checkPrefBot == true {
+		if receivedText != "" && checkWelcPref(receivedText, ev) != true {
+			rtm.SendMessage(rtm.NewOutgoingMessage("I'm sorry. I don't know it. I`m your friend. I can say \"Hello\" for you.", ev.Channel))
+		}
+	}
+}
+
+func checkWelcPref(receivedText string, ev *slack.MessageEvent) bool {
 	clientInfo, err := SlackClient.GetUserInfo(ev.User)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	if checkPrefBot == true {
-		for _, val := range welcPref {
-			checkRecivedTxt := strings.Contains(receivedText, val)
-			if checkRecivedTxt == true {
-				log.Printf("Message: %v\n", ev)
-				rtm.SendMessage(rtm.NewOutgoingMessage("Hello, "+"<@"+clientInfo.ID+">!", ev.Channel))
-			} else {
-				continue
-			}
-		}
-		if receivedText != ""{
-			rtm.SendMessage(rtm.NewOutgoingMessage("I'm sorry. I don't know it. I`m your friend. I can say \"Hello\" for you.", ev.Channel))
+	for _, val := range welcPref {
+		checkRecivedTxt := strings.Contains(receivedText, val)
+		if checkRecivedTxt == true {
+			log.Printf("Message: %v\n", ev)
+			rtm.SendMessage(rtm.NewOutgoingMessage("Hello, "+"<@"+clientInfo.ID+">!", ev.Channel))
+			return true
+		} else {
+			continue
 		}
 	}
+	return false
 }
